46a6fa8f36d65d71d0a505e71d77f129dbcf7465
胡鑫浩 <8962205@qq.com>
2024-08-23 16:19:50 +0800

    init

---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author lenovo
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2024-08-23 15:54:13
+*/
+public interface UserService extends IService<User> {
+
+}
